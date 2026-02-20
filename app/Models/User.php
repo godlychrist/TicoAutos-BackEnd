c6dfbdeaa6cd8d7f6cb31cbc3d6124a50b108766
Cristopher <cristopherquiros2@gmail.com>
2026-02-20 16:07:02 -0600

    added JWT token and connection to not use sql, use mongo instead

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,36 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    // Esto le dice a Laravel que no use SQL, sino la conexión Mongo que definiste
+    protected $connection = 'mongodb';
+    protected $collection = 'users'; 
+
     protected $fillable = [
-        'name',
-        'email',
+        'username',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function getJWTIdentifier()
+    {
+        return (string) $this->_id;    
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
