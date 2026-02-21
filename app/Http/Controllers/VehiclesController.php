cd68f6895a9d859731e400dbc434249528aa11b8
Cristopher <cristopherquiros2@gmail.com>
2026-02-20 20:12:14 -0600

    Vehicles Controller

---
+++ app/Http/Controllers/VehiclesController.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Vehicle;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; // Usaremos DB directamente
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class VehiclesController extends BaseController {
+
+    public function index() {
+        $vehicles = Vehicle::all();
+        return response()->json($vehicles);
+    }
+
+    public function store(Request $request) {
+
+        $request->validate([
+            'brand' => 'required|string|max:255',
+            'year' => 'required|integer|min:1900|max:2025',
+            'model' => 'required|string|max:255',
+            'price' => 'required|numeric|min:0',
+            'status' => 'required|string|in:available,sold',
+            'image' => 'required|image',
+            'user_id' => 'required|string'
+        ]);
+
+        $path = null;   
+        if ($request->hasFile('image')) {
+            $path = $request->file('image')->store('vehiculos', 'public');
+        }
+
+        $vehicle = DB::connection('mongodb')
+        ->collection('vehicles')
+        ->insert([
+            'brand' => $request->brand,
+            'year' => $request->year,
+            'model' => $request->model,
+            'price' => $request->price,
+            'status' => $request->status,
+            'image' => $path,
+            'user_id' => $request->user_id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return response()->json(['message' => 'Vehiculo agregado', 'vehicle' => $vehicle], 201);
+
+    }
+
+}
