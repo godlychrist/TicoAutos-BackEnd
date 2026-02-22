9153fe46e2f5170ffdaa8f6e54f306ef8c5dcf73
Cristopher <cristopherquiros2@gmail.com>
2026-02-22 15:07:20 -0600

    fixing db connection

--- config/database.php
+++ config/database.php
@@ -44,7 +44,7 @@ return [
         ],
         'mongodb' => [
             'driver' => 'mongodb',
-            'dsn' => env('DB_URI'),
+            'dsn' => env('MONGODB_URI'),
             'database' => env('DB_DATABASE', 'TicoCars'),
         ],
 
