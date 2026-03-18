74bbdae9cf1131f4b2b79c1029b16f27a9764bec
Brian Rodríguez <brianrp19@gmail.com>
2026-03-17 18:54:19 -0600

    feat: add MongoDB Eloquent models for Vehicle, Message, and User.

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,29 +5,31 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Jenssegers\Mongodb\Eloquent\Model;
 
+// Modelo para Mensajes Individuales dentro del Chat
 class Message extends Model
 {
-
     protected $connection = 'mongodb';
-    protected $collection = 'messages';
+    protected $collection = 'messages'; // Colección en MongoDB
     public $timestamps = false;
     
     use HasFactory;
 
+    // Datos necesarios para crear un mensaje
     protected $fillable = [
         'conversation_id',
         'sender_id',
         'message',
     ];
 
+    // Relación: Cada mensaje pertenece a un hilo de conversación
     public function conversation()
     {
         return $this->belongsTo(Conversation::class);
     }
 
+    // Relación: Cada mensaje tiene un usuario remitente
     public function sender()
     {
         return $this->belongsTo(User::class, 'sender_id');
     }
-
 }
